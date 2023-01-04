2be32fdacda8d36ab08782a20d4f7f5f091faeaa
Josa Gesell <josa@gesell.me>
2023-01-04 23:53:11 +0100

    refactor: remove usage of jquery to resize title

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,24 @@
 import '../plugins/bootstrap/js/bootstrap.min.js';
 
-import $ from '../plugins/jquery/jquery.js';
-
 const query = document.querySelector.bind(document)
 const queryAll = document.querySelectorAll.bind(document)
 const on = (el, event, fn) => el.addEventListener(event, fn)
 
-// Highlight the top nav as scrolling occurs
-$('body').scrollspy({
-    target: '.navbar-fixed-top',
-    offset: ($('.navbar-header').height() + 1)
+// Set resize title based on scroll position
+const nav = query('.navbar-fixed-top')
+const off = query('.navbar-header').offsetHeight
+const checkAffix = () => {
+    const top = window.scrollY <= off
+
+    nav.classList.toggle('affix-top', top)
+    nav.classList.toggle('affix', !top)
+}
+
+on(window, 'scroll', checkAffix)
+checkAffix()
+
+on(window, 'load', () => {
+    nav.classList.add('with-transitions')
 })
 
 // Closes the Responsive Menu on Menu Item Click
@@ -19,19 +28,12 @@ queryAll('.navbar-collapse ul li a').forEach((a) => {
     })
 })
 
-// Offset for Main Navigation
-$('#navFixed').affix({
-    offset: {
-        top: ($('.navbar-header').height() * 2)
-    }
-})
-
 // handle invites
 // https://talomini.de/#invite_token=xxxxxxxxxxxxxxxxxxxxxx
 if (location.hash.startsWith('#invite_token=')) {
      document.head.appendChild(Object.assign(document.createElement('script'), {
         src: 'https://identity.netlify.com/v1/netlify-identity-widget.js',
-        obeload: () => {
+        onload: () => {
             window.netlifyIdentity?.on("init", user => {
                 if (!user) {
                     window.netlifyIdentity.on("login", () => {
