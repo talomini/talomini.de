31cd0501790ef57967b12e43677b7058b202cf64
Josa Gesell <josa@gesell.me>
2024-11-17 15:35:51 +0100

    chore: update run script

--- run.sh
+++ run.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
 
-npx netlify-cms-proxy-server & hugo serve; kill $!
+args=()
+
+while test $# -gt 0; do
+  case "$1" in
+    -h|--help)
+      echo "Usage: ..."
+      echo ""
+      echo "Options"
+      echo "  -h|--help    Show help"
+      ;;
+    --preview)
+      args+=("--config" "config.toml,config.preview.toml")
+      shift
+      ;;
+    --draft)
+      args+=("--buildDrafts")
+      shift
+      ;;
+    *)
+      echo "[error] Unknown option: $1" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+npx netlify-cms-proxy-server & hugo serve "${args[@]}"; kill $!
 
